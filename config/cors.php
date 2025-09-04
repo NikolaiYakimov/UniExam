315f658f834123b259fd794e5a859dc359434096
NikolaiYakimov <niki024204@gmail.com>
2025-09-04 03:47:22 +0300

    test

---
+++ config/cors.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+        'login',
+        'logout',
+    ],
+
+    'allowed_methods' => ['*'],
+
+
+    'allowed_origins' => [
+        'http://localhost:3000',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    // ЗАДЪЛЖИТЕЛНО за Sanctum (cookies + credentials)
+    'supports_credentials' => true,
+];
